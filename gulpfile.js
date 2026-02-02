cae997065d21704099c5e31e599e50353a37c254
barry hayles <barry.hayles@lytical.com>
2026-02-02 07:08:37 -0600

    updated build scripts

--- gulpfile.js
+++ gulpfile.js
@@ -4,28 +4,42 @@
   please refer to your license agreement on the use of this file.
 */
 
-import _clean from 'gulp-clean';
-import _gulp from 'gulp';
-import _pump from 'pump';
-import _uglify from 'gulp-uglify-es';
+const _clean = require('gulp-clean');
+const _fs = require('node:fs/promises');
+const _gulp = require('gulp');
+const _pump = require('pump');
+const _uglify = require('gulp-uglify-es');
 
-export const post_build = _gulp.parallel(
-  (done) =>
-    _pump(
-      _gulp.src(['package.json', 'README.md', './src/types.d.ts']),
-      _gulp.dest('./dist'),
-      done,
-    ),
-  (done) =>
-    _pump(
-      _gulp.src('./dist/**/*.js', { dot: true }),
-      _uglify.default({
-        mangle: { keep_fnames: true },
-        output: { comments: 'some' },
-      }),
-      _gulp.dest('./dist'),
-      done,
-    ),
-  (done) =>
-    _pump(_gulp.src('./dist/**/*.map', { read: false }), _clean(), done),
+async function remove_package_json_scripts(done) {
+  const package = await require('./dist/package.json');
+  delete package.scripts;
+  await _fs.writeFile(
+    './dist/package.json',
+    JSON.stringify(package, null, 2),
+  );
+  done();
+}
+
+exports.post_build = _gulp.series(
+  _gulp.parallel(
+    (done) =>
+      _pump(
+        _gulp.src(['package.json', 'README.md', './src/**/*.d.ts']),
+        _gulp.dest('./dist'),
+        done,
+      ),
+    (done) =>
+      _pump(
+        _gulp.src('./dist/**/*.js', { dot: true }),
+        _uglify.default({
+          mangle: { keep_fnames: true },
+          output: { comments: 'some' },
+        }),
+        _gulp.dest('./dist'),
+        done,
+      ),
+    (done) =>
+      _pump(_gulp.src('./dist/**/*.map', { read: false }), _clean(), done),
+  ),
+  remove_package_json_scripts,
 );
